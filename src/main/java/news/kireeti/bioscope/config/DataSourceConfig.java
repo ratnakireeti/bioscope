86d461d245ddd2f9f6776f4b8db11b3b55b6c27e
Ratna Kireeti Eluri <ratnakireeti@gmail.com>
2018-02-14 21:57:45 -0600

    Reverting the changes

--- src/main/java/news/kireeti/bioscope/config/DataSourceConfig.java
+++ src/main/java/news/kireeti/bioscope/config/DataSourceConfig.java
@@ -42,7 +42,16 @@ public class DataSourceConfig extends AbstractMongoConfiguration {
 	@Override
 	@Bean
 	public Mongo mongo() throws Exception {
-		MongoClientURI uri = new MongoClientURI(env.getRequiredProperty("mongodb.uri"));	
+//		ServerAddress serverAddress = new ServerAddress(env.getRequiredProperty("mongo.host"), 31588);
+//		MongoCredential credential = MongoCredential.createMongoCRCredential("admin", "heroku_6f3xn8cd", "password".toCharArray());
+//		return new MongoClient(serverAddress, Arrays.asList(credential));
+		
+//		MongoClientURI uri = new MongoClientURI("mongodb://admin:k@cluster0-shard-00-00-bxbw3.mongodb.net:27017,"
+//								+ "cluster0-shard-00-01-bxbw3.mongodb.net:27017,"
+//								+ "cluster0-shard-00-02-bxbw3.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin");
+
+		
+		MongoClientURI uri = new MongoClientURI("mongodb://kireeti:kireeti@ds231588.mlab.com:31588/heroku_6f3xn8cd");	
 		return new MongoClient(uri);
 	}
 
